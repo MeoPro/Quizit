6783b1c378aa13db87622e8e87127cf3124d5140
MeoPro <107529661+MeoPro@users.noreply.github.com>
2022-06-16 00:42:03 +0700

    aaa

--- app/src/main/java/com/example/quiz_it/pengetahuankategori.kt
+++ app/src/main/java/com/example/quiz_it/pengetahuankategori.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.quiz_it.Pengetahuan.Bendera
 import com.example.quiz_it.databinding.ActivityPengetahuankategoriBinding
 
 class pengetahuankategori : AppCompatActivity() {
