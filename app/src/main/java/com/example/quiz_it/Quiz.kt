c3ad0bf713143579379fc51d1a14e7d30c06423a
MeoPro <107529661+MeoPro@users.noreply.github.com>
2022-06-15 13:16:26 +0700

    q

--- app/src/main/java/com/example/quiz_it/Quiz.kt
+++ app/src/main/java/com/example/quiz_it/Quiz.kt
@@ -7,5 +7,6 @@ class Quiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        
     }
 }
